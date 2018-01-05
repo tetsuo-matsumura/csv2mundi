55bd7606edf23c8481751d2d66cd31af7c75f5ab
Tetsuo <tet.m@hotmail.com>
2018-01-05 18:50:54 -0200

    Remodeled framework for Angular base. Purged EJS engine.

--- app.js
+++ app.js
@@ -1,43 +1,37 @@
+// set up ===================
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var Transaction = require('./api/models/transactionModel');
+var port = process.env.PORT || 8080;
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var setup = require('./setup.js');
+var methodOverride = require('method-override');
 
 // connect to mongo using mongoose
 mongoose.Promise = global.Promise;
 mongoose.connect(setup.mongourl); 
 var db = mongoose.connection;
-
-//connection error event
 db.on('error', console.error.bind(console, 'Failed to connect to MongoDB: '));
-
-//user routes
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 //api routes
 var routes = require('./api/routes/transactionRoutes');
 routes(app);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+// configurations ===================
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({ extended: 'true' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('X-HTTP-Method-Override')); 
 
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +50,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port);
+console.log("App listening on port " + port);
+
 
 module.exports = app;
