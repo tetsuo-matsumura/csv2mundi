b91ca36f2f5a92fd6896ee2e75ff9dc580cea662
Tetsuo <tet.m@hotmail.com>
2018-01-12 23:18:01 -0200

    Quick update on generate-csv. It wont generate lines with 'Hiper' brand and will also asign a 4-digit CVV to 'Amex' brand lines.

--- etc/tools/generate-csv/generate-csv.js
+++ etc/tools/generate-csv/generate-csv.js
@@ -21,17 +21,17 @@ function generateLine(){
 	var result = "";
 	//Coluna 1: Prioridade (0,1,2)
 	var c1 = rng(2);
-	//Coluna 2: Valor do Pagamento (0-10000000)
-	var c2 = rng(10000000);
+	//Coluna 2: Valor do Pagamento (0-10000)
+	var c2 = rng(100000);
 	//Coluna 3: Bandeira do cartão
-	var bandeiras = ["Visa","Mastercard","Hipercard","Amex","Diners","Elo","Hiper"]
-	var c3 = bandeiras[rng(6)];
+	var bandeiras = ["Visa","Mastercard","Hipercard","Amex","Diners","Elo"]
+	var c3 = bandeiras[rng(5)];
 	//Coluna 4: Numero do cartão
 	var c4 = "4111111111111111";
 	//Coluna 5: Mês de expiração
 	var c5 = rng(11)+1;
 	//Coluna 6: Ano de eexpiração do cartão
-	var c6d1 = rng(1)+1;
+	var c6d1 = 2;
 	var c6d2 = rng(9);
 	//Coluna 7: Nome do portador do cartão
 	var portadores = ["DONALD TRUMP","BILL GATES","ROGER WATERS","JACK WELSH","ANDRE STREET"]
@@ -40,7 +40,11 @@ function generateLine(){
 	var c8d1 = rng(9);
 	var c8d2 = rng(9);
 	var c8d3 = rng(9);
-	result = c1+","+c2+","+c3+","+c4+","+c5+","+c6d1+c6d2+","+c7+","+c8d1+c8d2+c8d3
+	if (c3 == "Amex"){
+		result = c1+","+c2+","+c3+","+c4+","+c5+","+c6d1+c6d2+","+c7+","+c8d1+c8d2+c8d2+c8d3;
+		return result;
+	}
+	result = c1+","+c2+","+c3+","+c4+","+c5+","+c6d1+c6d2+","+c7+","+c8d1+c8d2+c8d3;
 	return result;
 };
 
